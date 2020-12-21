e72d76b652ff35d90919f4f85c5eacf18682b21d
Moisés Navarro <moisalejandro@gmail.com>
2020-12-21 13:15:32 -0600

    [REF]

--- models/project_project.py
+++ models/project_project.py
@@ -1,17 +1,18 @@
 from odoo import models, fields
 
+
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     _sql_constraints = [
         (
-            'project_date_greater', 
-            'check(date >= date_start)',
-            'Error! project start-date must be lower than project end-date.'
+            "project_date_greater",
+            "check(date >= date_start)",
+            "Error! project start-date must be lower than project end-date.",
         ),
         (
-            'project_name_unique',
-            'UNIQUE(name)',
-            'Error! project name must be unique.'
+            "project_name_unique",
+            "UNIQUE(name)",
+            "Error! project name must be unique.",
         ),
     ]
